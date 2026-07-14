1a458ac6eaf36e79ccd16db11c049b07ab0085ad
Sejal Shetty <sejalvshetty@gmail.com>
2026-07-14 23:18:21 +0530

    Update cs.js

--- cs.js
+++ cs.js
@@ -1,74 +1,159 @@
-// Save selected semester
+// ELEMENTS
 
 const semesterSelect = document.getElementById("semesterSelect");
-loadSemesterSubjects();
+const subjectCards = document.querySelectorAll(".subject-card");
+const editableSubjects = document.querySelectorAll(".subject-card .editable");
+
+// LOAD SAVED SEM
 
 semesterSelect.value =
-localStorage.getItem("selectedSemester") || "1";
+    localStorage.getItem("selectedSemester")  || "1";
+
+//DEFAULT SUBJECTS
+
+const defaultSubjects = {
+    1: [
+        "Python",
+        "Computer Fundamentals",
+        "WebTech",
+        "Linux",
+        "EVS",
+        "Business Communication",
+        "OE A",
+        "OE B"
+    ],
+
+    2: [
+        "DAA",
+        "DBMS",
+        "Maths",
+        "Applied Python, R",
+        "EVS",
+        "Business Communication",
+        "OE A",
+        "OE B"
+    ],
+
+    3: [
+        "Subject 1",
+        "Subject 2",
+        "Subject",
+        "Subject 3",
+        "Subject 4",
+        "Subject 5",
+        "Subject 6",
+        "Subject 7",
+        "Subject 8",
+    ],
+
+    4: [
+        "Subject 1",
+        "Subject 2",
+        "Subject",
+        "Subject 3",
+        "Subject 4",
+        "Subject 5",
+        "Subject 6",
+        "Subject 7",
+        "Subject 8",
+    ],
+
+    5: [
+        "Subject 1",
+        "Subject 2",
+        "Subject",
+        "Subject 3",
+        "Subject 4",
+        "Subject 5",
+        "Subject 6",
+        "Subject 7",
+        "Subject 8",
+    ],
+
+    6: [
+        "Subject 1",
+        "Subject 2",
+        "Subject",
+        "Subject 3",
+        "Subject 4",
+        "Subject 5",
+        "Subject 6",
+        "Subject 7",
+        "Subject 8",
+
+// LOAD SUBJECTS
+
+function loadSemesterSubjects(){
+    const sem = semesterSelect.value;
+    editableSubjects.forEach((subject,index)=>{
+        const key = `sem${sem}_sub${index}`;
+        const saved = localStorage.getItem(key);
+        if(saved){
+            subject.innerHTML = saved;
+        }
+        else{
+            subject.innerHTML = defaultSubjects[sem][index];
+        }
+    });
+}
+
+
+// SAVE SUBJECTS
+
+function saveSemesterSubjects(){
+    const sem = semesterSelect.value;
+    editableSubjects.forEach((subject,index)=>{
+        const key = `sem${sem}_sub${index}`;
+        localStorage.setItem(
+            key,
+            subject.innerHTML
+        );
+    });
+}
+
+/* make function available to editable.js*/
+
+window.saveSemesterSubjects = saveSemesterSubjects;
+
+// CHANGE SEM
 
 semesterSelect.addEventListener("change",()=>{
-
     localStorage.setItem(
         "selectedSemester",
         semesterSelect.value
     );
-
     loadSemesterSubjects();
-
 });
 
-// Subject click
+// OPEN MONTH PG
 
-document.querySelectorAll(".subject-card").forEach(card => {
-
-   card.addEventListener("click", (e) => {
-
-    if (document.getElementById("editBtn").textContent.trim() === "Save") {
-        return;
-    }
-
-    const semester = semesterSelect.value;
-
-    const subject = card.querySelector(".editable").textContent.trim();
-
-    localStorage.setItem("currentSemester", semester);
-    localStorage.setItem("currentSubject", subject);
-
-    window.location.href = "months.html";
-});
-
-});
-
-function loadSemesterSubjects(){
-
-    const semester = semesterSelect.value;
-
-    document.querySelectorAll(".editable").forEach((element,index)=>{
-
-        const key = `sem${semester}_sub${index+1}`;
-
-        const saved = localStorage.getItem(key);
-
-        if(saved){
-
-            element.innerHTML = saved;
-
+subjectCards.forEach(card=>{
+    card.addEventListener("click",(e)=>{
+        if(editBtn.textContent==="Save"){
+            return;
         }
-
+        const subject =
+        card.querySelector(".editable").textContent.trim();
+        localStorage.setItem(
+            "currentSemester",
+            semesterSelect.value
+        );
+        localStorage.setItem(
+            "currentSubject",
+            subject
+        );
+        window.location.href="months.html";
     });
+});
 
-}
 
-function saveSemesterSubjects(){
+// INITIAL LOAD
 
-    const semester = semesterSelect.value;
+loadSemesterSubjects();
 
-    document.querySelectorAll(".editable").forEach((element,index)=>{
 
-        const key = `sem${semester}_sub${index+1}`;
 
-        localStorage.setItem(key,element.innerHTML);
 
-    });
 
-}
+
+        
